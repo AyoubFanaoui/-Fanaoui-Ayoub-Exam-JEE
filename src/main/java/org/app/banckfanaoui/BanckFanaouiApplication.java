0a9e0d03d6a3ec8152fc60cf832d87b84982583b
Ayoub <Fanaoui.25@gmail.com>
2025-05-19 10:22:13 +0200

    init

---
+++ src/main/java/org/app/banckfanaoui/BanckFanaouiApplication.java
@@ -0,0 +1,13 @@
+package org.app.banckfanaoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanckFanaouiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanckFanaouiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/app/banckfanaoui/BanckFanaouiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.app.banckfanaoui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanckFanaouiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
